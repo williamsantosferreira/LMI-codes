48bed3d212bad5c8c6f82817754206d411b4dc79
William Santos <williamsantostr@gmail.com>
2022-06-14 20:14:33 -0300

    balancing robot

--- Self_balancing.m
+++ Self_balancing.m
@@ -1,43 +1,51 @@
-%{
-D-stability code
-
-A região D-stability anilizada abaixo são de duas linhas verticais.
-Ou seja, é necessário que:
---> Seja encontrado polos dentro dessa região especificada
-
-Teoria:
-
-%}
-
+close all;
 clear;
 clc;
-H = tf(9,[1 4.8 9]); % process
+
+M = 0.112; %Massa da base (Rodas e Motores)
+m = 0.438; %Massa do pêndulo
+b = 0.1; %Atrito
+I = 0.00287; %Momento de Inercia
+g = 9.8; %Gravidade
+l = 0.03765; %Distância da base ao centro de massa
+
+q = [(M+m)*(I+m*l^2)-(m*l)^2];
+s = tf ('s');
+H = ((m*l*s)/q)/(s^3+b*(I+m*l^2)*s^2/q-(M+m)*m*g*l*s/q-b*m*g*l/q);
 [nH,dH] = tfdata(H,'v');
 [A,B,C,D] = tf2ss(nH,dH);
 G = ss(A,B,C,D);
 
-Z = sdpvar(1,length(A));
+x1 = -0.1;
+y1 = 1.33;
+y2 = -1.33;
+
 P = sdpvar(length(A));
-alfa = -2;
-beta =-4; 
+Z = sdpvar(1,length(B));
+Q = sdpvar(length(A));
+Y = sdpvar(1,length(B));
 
-F1 = [P >= 0, Z >= 0];
-F2 =[-2*alfa*P + A*P + P*A' + B*Z + Z'*B' <= 0]; %linha vertical < alfa
-%F3 =[2*beta*P + A*P + P*A' + B*Z + Z'*B' <= 0]; %linha vertical > beta
-F = [F1,F2]; 
-sol = solvesdp(F)
-Z = value(Z);
+F0 = [P >= 0, Q >= 0];
+F2 = [Q*A'+ A*Q + Y'*B' + B*Y <=0];
+F = [F0,F2];
+
+sol = solvesdp(F);
+
+%Z = value(Z);
 P = value(P);
+Y = value(Y);
+Q = value(Q);
 
-K = Z*inv(P);
+K = Y*inv(Q);
 
 Amf = A + B*K;
 Gmf = ss(Amf,B,C,D);
 U = ss(Amf,B,K,0);
 
-[Z,P,K] = ss2zp(Amf,B,C,D);
 rlocus(Gmf);
 figure
+[Z,P,K] = ss2zp(Amf,B,C,D);
+
 %% checking solution
 if sol.problem == 0
     [primal,~]=check(F); % Checking that the solver returned a proper solution
@@ -55,23 +63,18 @@ else
         disp(['LMIs not solved. Solver acused ' yalmiperror(sol.problem)]);
     end
 end
-
+opt = stepDataOptions('InputOffset',0,'StepAmplitude',1);
 subplot(1,3,1);
-step(G);
+step(G,opt);
 title('Open Loop');
 grid;
 
 subplot(1,3,2);
-step(Gmf);
+step(Gmf,opt);
 title('Closed Loop');
 grid;
 
 subplot(1,3,3);
-step(U);
+step(U,opt);
 title('Control Signal');
 grid;
-
-
-
-
-
